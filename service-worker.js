a7c397546d59a1decd2dcffbd39c1418ed211dfc
Wilkor <wilkorguitarrista@msn.com>
2024-05-21 10:29:31 -0300

    Melhorias
    Adicionando função para atualizar o listBots

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.aea94453e9b5ad684b07fa5bdbbd25b9.js"
+  "./precache-manifest.4c6ed20228abd1f1f1dfda4281f79885.js"
 );
 
 self.addEventListener('message', (event) => {
